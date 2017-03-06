480b3c17a3d169d715caf67e6482cd4bddab539d
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-03-05 16:57:49 -0800

    part1 complete, part2 in progress

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,86 @@
+// eslint-disable-next-line new-cap
+/* eslint-disable camelcase */
 'use strict';
 
 const express = require('express');
+const env = process.env.node_env || 'development';
+const config = require('../knexfile.js')[env];
+const knex = require('knex')(config);
 
-// eslint-disable-next-line new-cap
+const pg = require('pg');
+const bodyParser = require('body-parser');
+
 const router = express.Router();
 
-// YOUR CODE HERE
+router.use(bodyParser.json());
+
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+router.get('/books', (req, res, err) => {
+  knex('books').orderBy('title', 'asc')
+  .then((books) => {
+    res.status(200).send(books);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
+
+router.get('/books/:id', (req, res, err) => {
+  knex('books')
+  .where('id', '=', req.params.id)
+  .then((book) => {
+    res.status(200).send(book);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
+
+router.post('/books', (req, res, err) => {
+  knex('books')
+  .insert({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    coverUrl: req.body.cover_url,
+    createdAt: knex.fn.now(),
+    updatedAt: knex.fn.now()
+  }).then((book) => {
+    res.status(200).send(book);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
+
+router.patch('/books/:id', (req, res, err) => {
+  knex('books')
+  .where('id', '=', req.params.id)
+  .update({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.cover_url,
+    updatedAt: knex.fn.now()
+  }).then((book) => {
+    res.status(200).send(book);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
+
+router.delete('/books/:id', (req, res, err) => {
+  knex('books')
+  .where('id', '=', req.params.id)
+  .del()
+  .then((book) => {
+    res.status(200).send(book);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
 
 module.exports = router;
