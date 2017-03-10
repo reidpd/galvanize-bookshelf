ca45b0c3eb45bcd05d5816620174e2e898dd2f54
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-03-10 11:26:32 -0800

    part 4.1,2,3 complete

--- routes/token.js
+++ routes/token.js
@@ -12,51 +12,54 @@ const bcrypt = require('bcrypt-as-promised');
 const humps = require('humps');
 
 router.get('/token', (req, res, next) => {
-  next();
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err) => {
+    if (err) {
+      res.set('Content-Type', 'application/json');
+      res.status(200).send(false);
+    }
+    else {
+      res.set('Content-Type', 'application/json');
+      res.status(200).send(true);
+    }
+  });
 });
 
-router.post('/token', (req, res, done) => {
+router.post('/token', (req, res, next) => {
   knex('users').where('email', '=', req.body.email).then((userDataArray) => {
     const userInfo = userDataArray[0];
     bcrypt.compare(req.body.password, userInfo.hashed_password).then((userAuth) => {
-      console.log('compared!');
       if (userAuth) {
-        console.log('res turned true');
         const claim = { userId: req.body.email }; // this is our 'session'
-        console.log('we got a claim');
+
         const token = jwt.sign(claim, process.env.JWT_KEY, { // use this environment variable to sign the cookie
           expiresIn: '7 days'  // Adds an exp field to the payload
         });
-        console.log('token made');
+
         const opts = {
           httpOnly: true,
           expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),  // 7 days
           secure: router.get('env') === 'production'  // Set from the NODE_ENV
         };
-        console.log('cookie time...');
+
         res.cookie('token', token, opts);
-        done();
+        const responseObj = {
+          id: userInfo.id,
+          firstName: userInfo.first_name,
+          lastName : userInfo.last_name,
+          email: userInfo.email
+        }
+
+        res.status(200).send(responseObj);
       } else {
         res.status(200).send(false);
-        done();
       }
     });
   });
 });
 
-// app.use('/', (req, res) => {
-//   knex('users').where('email', '=', req.body.)
-// });
-
-
-//
-// router.post('/token', (req, res) => {
-//
-// });
-//
-// router.get('/token', (req, res) => {
-//
-// });
+router.delete('/token', (req, res) => {
+  
+});
 
 
 
