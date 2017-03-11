b8fa68b6b955e3256c2ca7835e7e1cab27f45dd7
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-03-10 16:20:21 -0800

    part 4.1,6,10 complete

--- migrations/20170310145835_favorites.js
+++ migrations/20170310145835_favorites.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('favorites', (table) => {
+    /*
+     id              │serial                   │primary key                                           │
+    │book_id         │integer                  │not null references books(id) on delete cascade index │
+    |user_id         │integer                  │not null references users(id) on delete cascade index │
+    │created_at      │timestamp with time zone │not null default now()                                │
+    │updated_at      │timestamp with time zone │not null default now() */
+    table.increments('id').primary();
+    console.log('id set');
+    table.integer('book_id').notNullable().references('id').inTable('books').onDelete('CASCADE');
+    console.log('book_id set');
+    table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
+    table.timestamps(true, true);
+    knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('favorites');
 };
--- routes/favorites.js
+++ routes/favorites.js
@@ -3,8 +3,54 @@
 const express = require('express');
 
 // eslint-disable-next-line new-cap
+const app = express();
 const router = express.Router();
+const knex = require('../knex.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt-as-promised');
+const humps = require('humps');
 
-// YOUR CODE HERE
+router.get('/favorites', (req, res, next) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err) => {
+    if (err) {
+      res.set('Content-Type', 'application/json');
+      return res.sendStatus(401);
+    }
+    knex('favorites')
+    .join('books', 'favorites.book_id', '=', 'books.id')
+    .then((favoritesArray) => {
+      res.set('Content-Type', 'application/json');
+      res.status(200).send(humps.camelizeKeys(favoritesArray));
+    });
+  });
+});
+
+//specific favorite check
+router.get('/favorites', (req, res, next) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err) => {
+    if (err) {
+      res.set('Content-Type', 'application/json');
+      return res.sendStatus(401);
+    }
+  });
+});
+
+router.post('/favorites/check', (req, res, next) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err) => {
+    if (err) {
+      res.set('Content-Type', 'application/json');
+      return res.sendStatus(401);
+    }
+  });
+});
+
+router.delete('/favorites', (req, res, next) => {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err) => {
+    if (err) {
+      res.set('Content-Type', 'application/json');
+      return res.sendStatus(401);
+    }
+  });
+});
 
 module.exports = router;
